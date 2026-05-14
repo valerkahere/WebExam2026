a16ec950a75620f04efe158341ba5ce7d27bd773
Valerii Navalnyi <valeranavalni@gmail.com>
2026-05-14 09:39:25 +0100

    Q(2): Add bootstrap, create components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('june-wp1');
 }
---
+++ src/app/components/countries/countries.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Countries } from './countries';
+
+describe('Countries', () => {
+  let component: Countries;
+  let fixture: ComponentFixture<Countries>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Countries],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Countries);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
