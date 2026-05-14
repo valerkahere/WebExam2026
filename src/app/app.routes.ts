c45b1a53d3d75d79f883ddfd6fb4f0a2affee9cf
Valerii Navalnyi <valeranavalni@gmail.com>
2026-05-14 09:43:09 +0100

    Q2(c): nav works

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { Countries } from './components/countries/countries';
+import { About } from './components/about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Countries},
+  {path: 'about', component: About}
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLinkWithHref, RouterLinkActive } from '@angular/router';
 
-
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, RouterLinkWithHref, RouterLinkActive],
