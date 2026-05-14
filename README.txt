1fd02b991376761d9b8762b10d61f212acdd72d4
Valerii Navalnyi <valeranavalni@gmail.com>
2026-05-14 11:00:29 +0100

    Q3(d): style the application

--- README.txt
+++ README.txt
@@ -1,2 +1,5 @@
-API endpoint:
+API endpoint: https://restcountries.com/v3.1/
+
+Full API endpoint used: https://restcountries.com/v3.1/all?fields=flag,name,capital,independent
+
 S3 public URL: 
--- src/app/services/exam-api/exam-api.ts
+++ src/app/services/exam-api/exam-api.ts
@@ -8,13 +8,11 @@ import { environment } from '../../../environments/environment.development';
   providedIn: 'root',
 })
 export class ExamApi {
-  private _http   = inject(HttpClient);
+  private _http = inject(HttpClient);
   private _apiURL = environment.apiURL;
 
   public countries = signal<Countries[] | null>([]);
   public apiError = signal<true | false>(false);
-
-
 
 
   getCountries() {
@@ -22,21 +20,21 @@ export class ExamApi {
 
     const fullURL = `${this._apiURL}all?fields=flag,name,capital,independent`;
     this._http.get<Countries[]>(fullURL)
-    .pipe(
-      // products side effects - preflight check that data arrived
-      tap((data) => console.log(`API data: ${JSON.stringify(data)}`))
-    )
-    .subscribe({
+      .pipe(
+        // products side effects - preflight check that data arrived
+        tap((data) => console.log(`API data: ${JSON.stringify(data)}`))
+      )
+      .subscribe({
         next: (dataArrived) => {
 
-            // console.log(data);
+          // console.log(data);
 
-            // api returns an array of js objects
-            // assign them directly to countries signal
-            this.countries.set(dataArrived);
-            console.log(`Assigned data: ${JSON.stringify(this.countries())}`);
+          // api returns an array of js objects
+          // assign them directly to countries signal
+          this.countries.set(dataArrived);
+          console.log(`Assigned data: ${JSON.stringify(this.countries())}`);
 
-            console.log(`One country name: ${JSON.stringify(this.countries()?.[1].name.common)}`);
+          console.log(`One country name: ${JSON.stringify(this.countries()?.[1].name.common)}`);
 
         },
         error: (data) => {
